bf649c287ad3b8d47df8621f1adbf9713bfbe81b
Yocss <yeahoor@qq.com>
2020-05-30 16:36:36 +0800

    空数据展示组件

--- src/components/base/_base-empty.vue
+++ src/components/base/_base-empty.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="base-empty">
-    <p>{{ title }}</p>
-    <van-button
-      v-if="button"
-      v-bind="button.attrs"
-      @click="$emit('click')"
-    >{{ button.buttonText }}</van-button>
+  <div class="base-empty flex-center">
+    <div class="base-empty-bd flex-column-center">
+      <slot>
+        <van-icon name="warning" />
+      </slot>
+      <p>{{ title }}</p>
+      <van-button
+        v-if="btn.show"
+        v-bind="btn.attrs"
+        @click="callBack"
+      >{{ btn.text }}</van-button>
+    </div>
   </div>
 </template>
 <script>
-import { Button } from 'vant'
+import { Button, Icon } from 'vant'
 export default {
   name: 'BaseEmpty',
   components: {
-    [Button.name]: Button
+    [Button.name]: Button,
+    [Icon.name]: Icon
   },
   props: {
     title: {
@@ -21,14 +27,14 @@ export default {
       default: '暂无相关数据'
     },
     button: {
-      type: [Object, Boolean],
+      type: [Object, String, Boolean],
       default: false
       // () => {
       //   return {
       //     attrs: {
       //       type: 'info',
       //     },
-      //     buttonText: '确定',
+      //     text: '确定',
       //   }
       // }
     },
@@ -36,6 +42,42 @@ export default {
       type: Function,
       default: undefined
     }
+  },
+  computed: {
+    btn () {
+      let show = false
+      let text = '确定'
+      const attrs = { size: 'small', type: 'info', plain: true, round: true }
+      const type = Object.prototype.toString.call(this.button).slice(8, -1)
+      switch (type) {
+        case 'Object':
+          show = true
+          Object.assign(attrs, this.button.attrs || {})
+          break
+        case 'String':
+          if (this.button) {
+            show = true
+            text = this.button
+          }
+          break
+      }
+      return Object.assign({}, { show, text, attrs })
+    }
   }
 }
 </script>
+
+<style lang="stylus">
+@import '~@/assets/css/common/variables.styl'
+.base-empty
+  width 100%
+  height 100%
+  &-bd
+    i
+      font-size 140px
+      color lighten($light, 60%)
+    p
+      margin $font auto
+      font-size $font
+      color $grey
+</style>
