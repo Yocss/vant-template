ccb3afe5217bca0e7d3060b657e61c68402b6e68
Yocss <yeahoor@qq.com>
2020-05-30 16:37:59 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="home-index">
+  <base-page
+    class="home-index"
+  >
     <router-link to="/news">
       去新闻页
     </router-link>
-  </div>
+  </base-page>
 </template>
 <script>
 export default {
   name: 'HomeIndex'
+  // computed: {
+  //   loading () {
+  //     return this.$store.state.loading
+  //   }
+  // },
+  // mounted () {
+  //   setInterval(() => {
+  //     this.$store.dispatch('SetStore', { loading: !this.loading })
+  //   }, 3000)
+  // }
 }
 </script>
 
 <style lang="stylus">
 .home-index
-  height 300px
+  // height 100%
 </style>
