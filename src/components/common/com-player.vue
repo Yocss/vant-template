c4d6e61177eef6f81de1c61c1ed39b5f39a5f58a
Yocss <yeahoor@qq.com>
2020-06-18 16:33:37 +0800

    ios设备不允许启用默认的全屏播放

--- src/components/common/com-player.vue
+++ src/components/common/com-player.vue
@@ -2,7 +2,7 @@
   <div class="com-player">
     <!-- 播放器 -->
     <div id="com-player-box">
-      <video-js
+      <video
         ref="videoPlayer"
         class="com-player-video video-js vjs-big-play-centered"
       />
@@ -10,8 +10,9 @@
   </div>
 </template>
 <script>
-import 'video.js/dist/video-js.css'
+import 'video.js/dist/video-js.min.css'
 import videojs from 'video.js'
+import { IS_IOS } from '@/assets/js/utils/platform.js'
 export default {
   name: 'ComPlayer',
   props: {
@@ -38,9 +39,12 @@ export default {
   },
   mounted () {
     const player = this.$refs.videoPlayer
+    // console.log(videojs.IS_IOS)
     const options = Object.assign({
-      autoplay: true,
+      autoplay: false,
       controls: true,
+      playsinline: IS_IOS, // 阻止IOS设备启用默认的全屏播放
+      webkitPlaysinline: IS_IOS, // 阻止IOS设备启用默认的全屏播放
       preload: 'auto',
       // children: [
       //   'bigPlayButton',
@@ -57,13 +61,15 @@ export default {
       // },
       // language: 'zh-CN',
       responsive: true,
-      // fluid: true,
-      // aspectRatio: '16:9',
+      fluid: true,
+      aspectRatio: '16:9',
       // poster: '//vjs.zencdn.net/(…)oceans.png',
       sources: '//vod.jiankao.wang/7c01465c94e449eeb2c795909d6b5eca/84b66563011d4411b801161ac54cd95a-6a3736091d286b946486bc6e0da0fdc7-sd.mp4'
     }, this.options)
+    const _this = this
     this.player = videojs(player, options, function onPlayerReady () {
       console.log('onPlayerReady', this)
+      _this.addEvent()
     })
   },
   beforeDestroy () {
@@ -93,22 +99,22 @@ export default {
     // 监听播放器事件
     addEvent () {
       // 元数据加载完成时
-      this.player.addListener('loadedmetadata', this.handleLoaded)
+      // this.player.addListener('loadedmetadata', this.handleLoaded)
       // 开始播放时，记录用户播放数据
-      this.player.addListener('play', this.handlePlay)
+      this.player.on('play', this.handlePlay)
       // 播放暂停
-      this.player.addListener('pause', this.handlePause)
+      this.player.on('pause', this.handlePause)
       // 发生错误时
-      this.player.addListener('error', this.handleError)
+      this.player.on('error', this.handleError)
       // 播放完成时
-      this.player.addListener('ended', this.handleEnded)
+      this.player.on('ended', this.handleEnded)
     },
     removeEvent () {
-      this.player.removeListener('loadedmetadata', this.handleLoaded)
-      this.player.removeListener('play', this.handlePlay)
-      this.player.removeListener('pause', this.handlePause)
-      this.player.removeListener('error', this.handleError)
-      this.player.removeListener('ended', this.handleEnded)
+      // this.player.off('loadedmetadata', this.handleLoaded)
+      this.player.off('play', this.handlePlay)
+      this.player.off('pause', this.handlePause)
+      this.player.off('error', this.handleError)
+      this.player.off('ended', this.handleEnded)
     },
     // 元数据加载完成时, 开始自动播放
     handleLoaded () {
@@ -117,16 +123,25 @@ export default {
     },
     // 播放开始时，开始记录学员的记录
     handlePlay () {
-      // console.log('play')
+      console.log('play')
       this.startLog()
       this.doEmit('play')
     },
     // 播放暂停
-    handlePause () { this.doEmit('pause') },
+    handlePause () {
+      console.log('pause')
+      this.doEmit('pause')
+    },
     // 播放发生错误时
-    handleError () { this.doEmit('error') },
+    handleError () {
+      console.log('error')
+      this.doEmit('error')
+    },
     // 播放结束时
-    handleEnded () { this.doEmit('ended') },
+    handleEnded () {
+      console.log('end')
+      this.doEmit('ended')
+    },
     // 记录用户播放时间进度
     startLog () {
       // 每 10s 触发一次记录
@@ -153,16 +168,37 @@ export default {
 .com-player
   display block
   width 100%
-  height 421.875px
   #com-player-box
     width 100%
     height 100%
     .com-player-video
-      width 100%
-      height 100%
-      font-size 10PX
-    video
-      display block
-      width 100%
-      height auto
+      font-size 12PX
+      .vjs-progress-holder
+        height .2em
+        user-select none
+        outline none
+        box-shadow none
+        &:focus
+          border none
+          box-shadow none
+      .vjs-play-progress
+        outline none
+        box-shadow none
+        &:focus
+          border none
+          box-shadow none
+        &:before
+          content ' '
+          display block
+          width 1em
+          height 1em
+          border-radius 50%
+          overflow hidden
+          background-color #fff
+          position absolute
+          right -0.5em
+          top 50%
+          margin-top -0.5em
+          z-index 1
+    // video
 </style>
