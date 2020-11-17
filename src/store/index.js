a83c2c4d93bf9ecfa2aaa640138f85bc81b65e48
Yocss <yeahoor@qq.com>
2020-11-17 18:10:46 +0800

    使用我的函数库，统一处理

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state.js'
-import mutations from './mutations.js'
+import { vuex, cookie as Cookie } from '@axmine/helper'
 import config from '@/app.config.js'
-import { setCookie, delCookie } from '@/assets/js/utils/cookie.js'
-
-const actions = {
-  SetStore ({ commit }, data) {
-    commit('SET_STORE', data)
-  },
+const cookie = new Cookie('sessionStorage')
+console.log(vuex)
+const actions = Object.assign(vuex.actions, {
   SetToken ({ commit }, token) {
     this.dispatch('SetStore', { token })
     const tk = config.keys.token || ''
     const t = config.keys.cookie || 0
     if (token) {
-      t && tk && setCookie(tk, token, t)
+      t && tk && cookie.set(tk, token, t)
       window.sessionStorage.setItem(tk, token)
     } else {
-      t && tk && delCookie(tk, token)
+      t && tk && cookie.remove(tk, token)
       window.sessionStorage.removeItem(tk)
     }
   }
-}
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  mutations,
+  mutations: vuex.mutations,
   actions
   // modules: {}
 })
