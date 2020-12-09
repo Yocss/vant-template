913e1647d0f891143fef4266ee89e0509158c9c4
Yocss <yeahoor@qq.com>
2020-12-09 16:27:05 +0800

    修改vuex配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state.js'
-import { vuex, cookie as Cookie } from '@axmine/helper'
+import { cookie as Cookie } from '@axmine/helper'
+import mutations from './mutations.js'
+
 import config from '@/app.config.js'
-const cookie = new Cookie('sessionStorage')
-console.log(vuex)
-const actions = Object.assign(vuex.actions, {
+const session = new Cookie('sessionStorage')
+const actions = {
+  SetStore ({ commit }, data) {
+    commit('SET_STORE', data)
+  },
   SetToken ({ commit }, token) {
     this.dispatch('SetStore', { token })
     const tk = config.keys.token || ''
     const t = config.keys.cookie || 0
     if (token) {
-      t && tk && cookie.set(tk, token, t)
+      tk && session.set(tk, token, t)
       window.sessionStorage.setItem(tk, token)
     } else {
-      t && tk && cookie.remove(tk, token)
+      tk && session.remove(tk, token)
       window.sessionStorage.removeItem(tk)
     }
   }
-})
+}
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  mutations: vuex.mutations,
+  mutations,
   actions
   // modules: {}
 })
